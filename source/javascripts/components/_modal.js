34e0c4e8e5670befe0f9a4998c86dfceee51824e
Ben Hays <ben@kajabi.com>
2018-10-05 11:33:11 -0700

    Made modal partial. Fixed script to add touchend.

--- source/javascripts/components/_modal.js
+++ source/javascripts/components/_modal.js
@@ -1,25 +1,40 @@
 (function($) {
 
   var $window = $(window);
-  var $body = $("body");
   var $modal = $(".modal");
+  var $modalPositioner = $(".modal__positioner");
   var $catalyst = $(".btn");
   var $close = $(".close-button");
 
   function open() {
     $modal.addClass("modal--open");
-    $body.addClss("overflow--hidden");
   }
 
   function close() {
     $modal.removeClass("modal--open");
-    $body.removeClass("overflow--hidden");
   }
 
-  $catalyst.click(open);
-  $close.click(close);
-  $window.click(function(event) {
-    console.log("event.target:", event.target, "event.currentTarget:", event.currentTarget);
+  function eventHandler(event, selector) {
+    event.stopPropagation();
+    event.preventDefault();
+    if (event.type === "touchend") selector.off("click");
+  }
+
+  $catalyst.on("touchend click", function(event) {
+    eventHandler(event, $(this));
+    open();
+  });
+
+  $close.on("touchend click", function(event) {
+    eventHandler(event, $(this));
+    close();
+  });
+
+  $window.on("touchend click", function(event) {
+    console.log("event.target:", event.target);
+    if ($(event.target).is($modalPositioner)) {
+      close();
+    }
   });
 
 })(jQuery);
