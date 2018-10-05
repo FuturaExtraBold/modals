7779a552e2d863afa18b01af47293c7aa5303c7a
Ben Hays <ben@kajabi.com>
2018-10-05 10:27:06 -0700

    Added simple version.

--- source/javascripts/components/_modal.js
+++ source/javascripts/components/_modal.js
@@ -1,49 +1,25 @@
-// // Get the modal
-// var modal = document.getElementById('myModal');
-//
-// // Get the button that opens the modal
-// var btn = document.getElementById("myBtn");
-//
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
-//
-// // When the user clicks on the button, open the modal
-// btn.onclick = function() {
-//   modal.style.display = "block";
-// };
-//
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function() {
-//   modal.style.display = "none";
-// };
-//
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//   if (event.target == modal) {
-//     modal.style.display = "none";
-//   }
-// };
 (function($) {
 
   var $window = $(window);
+  var $body = $("body");
   var $modal = $(".modal");
   var $catalyst = $(".btn");
   var $close = $(".close-button");
 
   function open() {
     $modal.addClass("modal--open");
+    $body.addClss("overflow--hidden");
   }
 
   function close() {
     $modal.removeClass("modal--open");
+    $body.removeClass("overflow--hidden");
   }
 
   $catalyst.click(open);
   $close.click(close);
-  $window.click(function() {
-    if (event.target == $modal) {
-      close();
-    }
+  $window.click(function(event) {
+    console.log("event.target:", event.target, "event.currentTarget:", event.currentTarget);
   });
 
 })(jQuery);
